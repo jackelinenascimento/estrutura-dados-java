78804e2b2a130282c27d488ffeb09a79b2355ebc
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2022-01-29 06:32:37 -0300

    aula 12

--- estruturadados-algoritmos-java/src/estruturadados/vetor/teste/Aula12.java
+++ estruturadados-algoritmos-java/src/estruturadados/vetor/teste/Aula12.java
@@ -6,8 +6,25 @@ public class Aula12 {
 
 	public static void main(String[] args) {
 
-		ArrayList<E>
+		ArrayList<String> arraylist = new ArrayList<String>();
+		
+		arraylist.add("A");
+		arraylist.add("B");
+		
+		System.out.println(arraylist);
 
+		arraylist.add(2, "C");
+		
+		System.out.println(arraylist);
+
+		boolean existe = arraylist.contains("A");
+		
+		System.out.println(existe);
+		
+		int pos = arraylist.indexOf("B");
+		
+		System.out.println(pos);
+		
 	}
 
 }
